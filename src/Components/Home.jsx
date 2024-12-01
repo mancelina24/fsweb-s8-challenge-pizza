a5e27b7c772a723692b8a4f05c9913b7bfd516ac
mancelina24 <feyza.turkay@ogr.iu.edu.tr>
2024-12-02 01:39:01 +0300

    2

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../App.css";
 
 const Home = () => {
   return (
     <>
-      <h2>KOD ACIKTIRIR</h2>
-      <h2>PİZZA, DOYURUR</h2>
+      <header class="order-header">
+        <h1>Teknolojik Yemekler</h1>
+      </header>
+      <div class="home">
+        <h2>KOD ACIKTIRIR</h2>
+        <h2>PİZZA, DOYURUR</h2>
 
-      <Link to="/OrderForm">
-        <button>ACIKTIM</button>
-      </Link>
+        <Link to="/OrderForm">
+          <button>ACIKTIM</button>
+        </Link>
+      </div>
     </>
   );
 };
--- src/Components/OrderForm.jsx
+++ src/Components/OrderForm.jsx
@@ -1,31 +1,83 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import "./OrderForm.css";
+import data from "../assets/data";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 const OrderForm = () => {
   return (
     <>
-      <div>
+      <header class="order-header">
         <h1>Teknolojik Yemekler</h1>
-        <Link to="/">Anasayfa</Link>
-        <Link to="/">Seçenekler</Link>
-        <Link to="/orderform">Sipariş Oluştur</Link>
-      </div>
-      <div>
+        <NavLink activeStyle={{ color: "green" }} exact to="/">
+          Anasayfa
+        </NavLink>
+        <NavLink activeStyle={{ color: "green" }} exact to="/">
+          Seçenekler
+        </NavLink>
+        <NavLink activeStyle={{ color: "green" }} exact to="/orderform">
+          Sipariş Oluştur
+        </NavLink>
+      </header>
+      <header>
         <div>Position Absolute Acı Pizza</div>
         <div>
-          <div>85.50-TL</div>
+          <div>85.50₺</div>
           <div>
             <span class="rating">
               <i class="fa-solid fa-star"></i>
               <i class="fa-solid fa-star"></i>
               <i class="fa-solid fa-star"></i>
               <i class="fa-solid fa-star"></i>
-              <i class="fa-regular fa-star"></i> 4.8
+              <i class="fa-regular fa-star"></i> 4.9
             </span>
             <span>(200)</span>
           </div>
         </div>
-      </div>
+      </header>
+      <main>
+        <div>
+          <p>
+            Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı
+            pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli
+            diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun
+            ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle
+            yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan
+            kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta
+            denir.
+          </p>
+        </div>
+        <div>
+          <div>
+            <label htmlFor="boyut">
+              <h3>Boyut Seç</h3>
+            </label>
+          </div>
+          <div>
+            <h3>Hamur Seç</h3>
+            <select
+              name="hamur"
+              onchange={(e) => {
+                e.target.value;
+              }}
+              value={data.hamur}
+            >
+              <option value="">Hamur Kalınlığı</option>
+              <option value="İnce">İnce</option>
+              <option value="Normal">Normal</option>
+              <option value="Kalın">Kalın</option>
+            </select>
+          </div>
+        </div>
+        <div>
+          <h3>Ek malzemeler</h3>
+          <p>En fazla 10 malzeme seçebilirsiniz 5₺</p>
+        </div>
+        <form action="">
+          <h3>Sipariş Notu</h3>
+          <input type="text" />
+        </form>
+        <div>adet ve sipariş toplam</div>
+      </main>
     </>
   );
 };
